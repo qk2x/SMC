767bf30d4d233bd04c070f66663eda525714f3c3
qingxin <qingxin@thinkplay.co>
2024-07-18 15:23:39 +0800

    建筑结构

---
+++ SMC_Client/Assets/Game/Map/Entity/BigMapStructData.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.Map.Entity
+{
+	public enum BigMapStructType
+	{
+		/// <summary>
+		/// 存储物资，并可以分发
+		/// </summary>
+		Warehouse,
+	}
+	
+	public struct BigMapStructData
+	{
+		
+	}
+}
